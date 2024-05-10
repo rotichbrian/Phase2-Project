a061836daafba6fd5fb66edb85df5b5b08f5dcb4
Brian Kipkorir <rotichbrian891@gmail.com>
2024-05-10 14:29:14 +0300

    implement styling

--- src/components/TaskEditor.js
+++ src/components/TaskEditor.js
@@ -1,4 +1,7 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 const TaskEditor = ({tasks}) => {
 
