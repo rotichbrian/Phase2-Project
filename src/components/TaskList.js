36318b4d544efeb158115fc8be35e5cb8ba7fba2
Brian Kipkorir <rotichbrian891@gmail.com>
2024-05-10 11:55:05 +0300

    Implement state and handlers for sorting and filtering task

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,6 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react';
+import {Link} from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
-const TaskList = () => {
+function TaskList({tasks}) {
+    //   const [id] = useParams()  
+  const [sortBy, setSortBy] = useState([]);
+  const [filterBy, setFilterBy] = useState([]);
+  const [sortOrder, setSortOrder] = useState('asc');
+
+  
+  const handleSort = (criteria) => {
+    setSortBy(criteria);
+    setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc');
+  };
+
+  const handleFilter = (criteria) => {
+    setFilterBy(criteria);
+  };
+
+
   return (
     <div>TaskList</div>
   )
